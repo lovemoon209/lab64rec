3c9e6a718eef15040981b6864dc54335d762acfd
lovemoon209 <liubov.lunha.ri.2025@lpnu.ua>
2025-10-22 19:50:30 +0300

    Initial commit: lab52 base

--- lab55.cpp
+++ lab55.cpp
@@ -11,10 +11,22 @@ else
      return  C(n - 1, k - 1) + C(n -1, k);
 }
 
-int f(int n){
+int f(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
-    if (n>1) return f(n-1) + f(n-2);
+    return f(n-1) + f(n-2);
+}
+
+int f(int N, int i, int t, int level, int &depth)
+{
+    if (level > depth)
+        depth = level;
+
+    cout << "level = " << level << endl;
+    if (level == N)
+        return depth;
+    else 
+    return f(N, i, t, level + 3, depth);
 }
 
 int main()
@@ -27,6 +39,10 @@ int main()
     for (int k=0; k<= n/2; k++){
  S += C(n - k, k);
     }
+
+    int depth = 0;
+    f(15, 0, 0, 3, depth);
+    cout << "depth: " << depth << endl;
     cout << "S = " << S << endl;
     cout << "C = " << C(n, k) <<  endl;
     cout <<"f = "<< f(n) << endl;
---
+++ lab61it.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int s(int mas[], const int i);
+
+int main(){
+const int i = 25;
+int p[i] = {15, .. , 94 };
+for (int i=0; i<l; i++){
+    cout << "p[" << i << "] = ";
+    cin >> p[i];
+}
+
+for (int i=0; i<l; i++)
+cout << p[i] << " ";
+cout << endl;
+return 0;
+
+int s(int mas[], const int i){
+    int s=0;
+    for (int i=0; i<l; i++);
+    s += mas[i]
+}
+
+if (!(p[i]%2=0 && i%13=0)){
+    s += p[i];
+    k++;
+    p[i]=0;
+}
+return s;
+}
